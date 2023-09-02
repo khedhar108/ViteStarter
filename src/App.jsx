3c7638573a730386b56d9c15d2d49ae9c17266b3
khedhar108 <khedhar.pradeep108@gmail.com>
2023-09-02 17:01:33 +0530

    viteProjectSetup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen text-3xl flex justify-center items-center">
+        <h1>Start your Vite React project with tailwind css using pnpm</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
